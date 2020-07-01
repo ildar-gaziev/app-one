e4aa57543b8ef4639f53dc9b53b4683a690ed635
ildar gaziev <ildar@gaziev.info>
2020-07-02 02:11:04 +0300

    testify

--- count/count_test.go
+++ count/count_test.go
@@ -1,11 +1,15 @@
 package count
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCount(t *testing.T) {
-	s := "qwertyeege"
-	e := 4
-	if c := Count(s, 'e'); c != e {
-		t.Fatalf("bad count for %s: got %d expected %d", s, c, e)
-	}
+	s := "qwerrtyeege"
+
+	require.Equal(t, Count(s, 'e'), 4, "counting 'e' in " + s)
+	require.Equal(t, Count(s, 'r'), 2, "counting 'r' in " + s)
+	require.Equal(t, Count(s, 'q'), 1, "counting 'q' in " + s)
+
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module mod
 
-require github.com/ildar-gaziev/app-one v1.0.0
+require (
+	github.com/ildar-gaziev/app-one v1.0.0
+	github.com/stretchr/testify v1.6.1
+)
 
 go 1.13
