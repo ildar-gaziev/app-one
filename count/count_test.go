0ebe6f3f08c4fa54c75b7bf6033b996e627e0788
ildar gaziev <ildar@gaziev.info>
2020-07-02 02:30:35 +0300

    test edited

--- count/count_test.go
+++ count/count_test.go
@@ -1,15 +1,15 @@
 package count
 
 import (
-	"testing"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestCount(t *testing.T) {
 	s := "qwerrtyeege"
 
-	require.Equal(t, Count(s, 'e'), 4, "counting 'e' in " + s)
-	require.Equal(t, Count(s, 'r'), 2, "counting 'r' in " + s)
-	require.Equal(t, Count(s, 'q'), 1, "counting 'q' in " + s)
+	require.Equal(t, Count(s, 'e'), 4, "counting 'e' in "+s)
+	require.Equal(t, Count(s, 'r'), 2, "counting 'r' in "+s)
+	require.Equal(t, Count(s, 'q'), 1, "counting 'q' in "+s)
 
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module mod
 require (
 	github.com/ildar-gaziev/app-one v1.0.0
 	github.com/stretchr/testify v1.6.1
+
 )
 
 go 1.13
